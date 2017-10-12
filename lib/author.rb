7451abb94d983a6a9cba5f09da3768428cdac663
Kristen Peck <kpeck71@gmail.com>
2017-10-12 11:44:52 +0000

    automatically backed up by learn

---
+++ lib/author.rb
@@ -0,0 +1,34 @@
+require 'pry'
+
+class Author
+  attr_accessor :name, :post_count
+
+  @@post_count = 0
+
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+
+  def posts
+    @posts
+  end
+
+  def add_post(post)
+    @posts << post
+    @@post_count += 1
+    post.artist = self
+  end
+
+  def add_post_by_name(name)
+    post = post.new(name)
+    @posts << post
+    @@post_count += 1
+    .artist = self
+  end
+
+  def self.post_count
+    @@post_count
+  end
+
+end
