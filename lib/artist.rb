b99adbdc24b291f6acee2e33db04754116308fbb
Kristen Peck <kpeck71@gmail.com>
2017-10-12 11:40:01 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,10 +3,11 @@ require 'pry'
 class Artist
   attr_accessor :name, :song_count
 
+  @@song_count = 0
+
   def initialize(name)
     @name = name
     @songs = []
-    @song_count = 0
   end
 
   def songs
